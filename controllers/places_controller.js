639b4a9ea922f72748c6b802753d9f45894b1da3
Adnan <adnanvandyck@gmail.com>
2023-06-21 18:45:50 -0400

    Show route connected

--- controllers/places_controller.js
+++ controllers/places_controller.js
@@ -6,7 +6,7 @@ router.get('/new', (req, res) => {
 })
 
 router.get('/', (req, res) => {
-   
+
     res.render('places/index', {places})
 })
 
@@ -25,6 +25,17 @@ router.post('/', (req, res) => {
   res.redirect('/places')
 });
 
-
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)){
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+  res.render('places/show', { place: places[id] })
+  }
+})
 
 module.exports = router
