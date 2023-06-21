a596fe710992cc88b0b7f315bd27e83e36e2007c
Adnan <adnanvandyck@gmail.com>
2023-06-21 18:16:47 -0400

    Navbar with style

--- controllers/places_controller.js
+++ controllers/places_controller.js
@@ -6,20 +6,7 @@ router.get('/new', (req, res) => {
 })
 
 router.get('/', (req, res) => {
-    // let places = [{
-    //     name: 'H-Thai-ML',
-    //     city: 'Seattle',
-    //     state: 'WA',
-    //     cuisines: 'Thai, Pan-Asian',
-    //     pic: '/images/noodles.jpg'
-    //   }, {
-    //     name: 'Coding Steakhouse',
-    //     city: 'Phoenix',
-    //     state: 'AZ',
-    //     cuisines: 'Steakhouse',
-    //     pic: '/images/tomahawk.jpg'
-    //   }]
-      
+   
     res.render('places/index', {places})
 })
 
--- views/home.jsx
+++ views/home.jsx
@@ -7,7 +7,7 @@ function home () {
             <main>
                 <h1>REST-Rant</h1>
                 <div>
-                    <img src="/images/grilled-bbq.jpg" alt="Grilled BBQ" />
+                    <img className="img-fluid height:50% rounded mx-auto d-block" src="/images/grilled-bbq.jpg" alt="Grilled BBQ" />
                 </div>
                 <div>
                 Photo by <a href="https://unsplash.com/@victoriakosmo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Victoria Shes</a> on <a href="https://unsplash.com/images/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
