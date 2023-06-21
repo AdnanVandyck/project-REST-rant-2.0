c50c6834455fd8e12b4a9e26ea7e48e2e58c8263
Adnan <adnanvandyck@gmail.com>
2023-06-21 00:26:58 -0400

    Reverting to 28cffe2 - because moving controller index info crashed app

--- controllers/places.js
+++ controllers/places.js
@@ -1,20 +1,26 @@
-const router = require('express').Router();
-const places = require('..models/places.js');
+const router = require('express').Router()
 
 router.get('/new', (req, res) => {
     res.render('places/new')
 })
 
-router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST /places')
-})
-
 router.get('/', (req, res) => {
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: '/images/noodles.jpg'
+      }, {
+        name: 'Coding Steakhouse',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Steakhouse',
+        pic: '/images/tomahawk.jpg'
+      }]
+      
     res.render('places/index', {places})
 })
-
-
 
 
 module.exports = router
